de35bc955d623d2438b556ae1366b4fd2473f14c
bnngcall36 <bongcall1215@gmail.com>
2023-02-22 15:24:58 +0900

    update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+
 const path = require('path');
 
 const toPath = (_path) => path.join(process.cwd(), _path);
@@ -9,12 +10,17 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y'
   ],
   core: {
     builder: {
       name: 'webpack5',
     },
   },
+ features: {
+   interactionsDebugger: true,
+ },
   webpackFinal: async (config) => {
     return {
       ...config,
